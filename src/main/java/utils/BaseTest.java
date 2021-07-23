d6cee0d52be40e255e070f3b69689463ef5047fc
Lucas Sebastian de la Fuente <55161369+lucassdelafuente@users.noreply.github.com>
2021-07-23 11:55:20 -0300

    Add files via upload

---
+++ src/main/java/utils/BaseTest.java
@@ -0,0 +1,20 @@
+package utils;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+
+    WebDriver driver;
+
+    @Before
+    {
+
+    }
+
+    @After
+    {
+
+    }
+}
